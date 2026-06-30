853fc011f6d679767d8130baad54ec23dbfeffb5
Yash Korane <koraneyash92@gmail.com>
2026-06-30 18:46:47 +0530

    feat: initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+        Welcome
+      </h1>
+    </div>
+  );
+}
